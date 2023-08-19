7e0dae42eec43706bba15876d096bdbb4429fec7
soda-sy <qkrthdud2124@naver.com>
2023-08-19 23:39:56 +0900

    classList delete 오류 수정

--- app/src/main/java/nepal/swopnasansar/admin/ClassListActivity.kt
+++ app/src/main/java/nepal/swopnasansar/admin/ClassListActivity.kt
@@ -10,13 +10,22 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import nepal.swopnasansar.R
 import nepal.swopnasansar.data.AccountantDto
 import nepal.swopnasansar.data.RvClassListDto
+import nepal.swopnasansar.data.SubjectDto
+import nepal.swopnasansar.data.TeacherDto
 import nepal.swopnasansar.databinding.ActivityAccountantListBinding
 import nepal.swopnasansar.databinding.ActivityClassListBinding
+import nepal.swopnasansar.dto.Homework
+import javax.security.auth.Subject
 
 class ClassListActivity : AppCompatActivity() {
     lateinit var binding : ActivityClassListBinding
@@ -33,6 +42,8 @@ class ClassListActivity : AppCompatActivity() {
         val classList = ArrayList<RvClassListDto>()
         val pref : SharedPreferences = getSharedPreferences("save_state", 0)
         val editor : SharedPreferences.Editor = pref.edit()
+        var subjectList = ArrayList<SubjectDto>()
+        var homeworkList = ArrayList<Homework>()
 
         adapter = ClassAdapter(this, classList)
         binding.rvClassList.adapter = adapter
@@ -50,25 +61,79 @@ class ClassListActivity : AppCompatActivity() {
                     setCancelable(false)
                     setPositiveButton("Yes", object: DialogInterface.OnClickListener {
                         override fun onClick(p0: DialogInterface?, p1: Int) {
-                            db.collection("class").document(classList[position].class_key).delete()
-                                .addOnSuccessListener {
-                                    Log.d(TAG, "delete success")
-                                }
-                                .addOnFailureListener { e ->
-                                    // 삭제 중에 발생한 오류 처리
-                                    Toast.makeText(this@ClassListActivity, "delete failed: ${e.message}", Toast.LENGTH_SHORT).show()
+                            CoroutineScope(Dispatchers.IO).launch {
+                                // 과목 정보
+                                subjectList.clear()
+                                val subjectQuerySnapshot = db?.collection("subject")
+                                    ?.whereEqualTo("class_key", classList[position].class_key)?.get()?.await()
+                                subjectList.addAll(
+                                    subjectQuerySnapshot?.toObjects(SubjectDto::class.java)
+                                        ?: emptyList()
+                                )
+
+                                // 숙제 정보
+                                homeworkList.clear()
+                                val homeworkQuerySnapshot = db?.collection("homework")
+                                    ?.whereEqualTo("class_key", classList[position].class_key)?.get()?.await()
+                                homeworkList.addAll(
+                                    homeworkQuerySnapshot?.toObjects(Homework::class.java)
+                                        ?: emptyList()
+                                )
+
+                                for(s in subjectList){
+                                    Log.d(TAG, "${s.subject_key}")
+                                    db.collection("subject").document(s.subject_key)
+                                        .delete()
+                                        .addOnSuccessListener {
+                                         Log.d(TAG, "delete success!!")
+                                        }
+                                        .addOnFailureListener { e ->
+                                            // 삭제 중에 발생한 오류 처리
+                                            Toast.makeText(
+                                                this@ClassListActivity,
+                                                "delete failed: ${e.message}",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }.await()
                                 }
 
-                            db.collection("subject").document(classList[position].subject_key).delete()
-                                .addOnSuccessListener {
-                                    classList.removeAt(position) // ArrayList에서 항목 삭제
-                                    adapter.notifyDataSetChanged() // 어댑터에 데이터 변경 알림
-                                    Toast.makeText(this@ClassListActivity, "delete success!!", Toast.LENGTH_SHORT).show()
+                                for(h in homeworkList){
+                                    Log.d(TAG, "${h.homework_key}")
+                                    db.collection("homework").document(h.homework_key)
+                                        .delete()
+                                        .addOnSuccessListener {
+                                            Log.d(TAG, "delete success!!")
+                                        }
+                                        .addOnFailureListener { e ->
+                                            // 삭제 중에 발생한 오류 처리
+                                            Toast.makeText(
+                                                this@ClassListActivity,
+                                                "delete failed: ${e.message}",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }.await()
                                 }
-                                .addOnFailureListener { e ->
-                                    // 삭제 중에 발생한 오류 처리
-                                    Toast.makeText(this@ClassListActivity, "delete failed: ${e.message}", Toast.LENGTH_SHORT).show()
-                                }
+
+                                db.collection("class").document(classList[position].class_key)
+                                    .delete()
+                                    .addOnSuccessListener {
+                                        classList.removeAt(position) // ArrayList에서 항목 삭제
+                                        adapter.notifyDataSetChanged() // 어댑터에 데이터 변경 알림
+                                        Toast.makeText(
+                                            this@ClassListActivity,
+                                            "delete success!!",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                    .addOnFailureListener { e ->
+                                        // 삭제 중에 발생한 오류 처리
+                                        Toast.makeText(
+                                            this@ClassListActivity,
+                                            "delete failed: ${e.message}",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }.await()
+                            }
                         }
                     })
                     show()
