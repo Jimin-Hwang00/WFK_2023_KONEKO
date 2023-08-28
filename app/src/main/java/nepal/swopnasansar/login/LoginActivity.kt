ed63d8f0efb9b317d9286a92e576a5416e1ac9eb
Jimin-Hwang00 <hwang0010@naver.com>
2023-08-28 17:12:11 +0900

    이메일 인증 절차 삭제

--- app/src/main/java/nepal/swopnasansar/login/LoginActivity.kt
+++ app/src/main/java/nepal/swopnasansar/login/LoginActivity.kt
@@ -79,13 +79,12 @@ class LoginActivity: AppCompatActivity() {
                 }
 
                 if (temp != null) {       // temp에 해당 계정이 있는 경우
-                    if (temp.size == 1) {   // temp에 일치하는 개정이 한 개만 있을 경우
-                        val loginResult = withContext(Dispatchers.IO) {
-                            authDao.login(email, pw)
-                        }
-
-                        if (loginResult) {  // auth에 들록된 사용자
-                            if (authDao.getUser()!!.isEmailVerified) {      // 이메일 인증이 완료된 경우
+                    if (temp.size == 1) {
+                        if (checkIsItFirstPW(email, pw)) {      // 로그인 성공
+                            val registerResult = withContext(Dispatchers.IO) {
+                                authDao.registerUser(email, pw)
+                            }
+                            if (registerResult) {
                                 val removeTempResult = withContext(Dispatchers.IO) {
                                     tempDao.removeTempData(temp[0].email)
                                 }
@@ -96,11 +95,11 @@ class LoginActivity: AppCompatActivity() {
                                     if (uid != null) {
                                         val createUserDBResult = withContext(Dispatchers.IO) {
                                             if (role.equals(getString(R.string.accountant))) {
-                                                accountantDao.createAccountantByUid(uid!!, temp[0])
+                                                accountantDao.createAccountantByUid(uid, temp[0])
                                             } else if (role.equals(getString(R.string.teacher))) {
-                                                teacherDao.createTeacherByUid(uid!!, temp[0])
+                                                teacherDao.createTeacherByUid(uid, temp[0])
                                             } else {
-                                                studentDao.createStudentByUid(uid!!, temp[0])
+                                                studentDao.createStudentByUid(uid, temp[0])
                                             }
                                         }
 
@@ -143,47 +142,13 @@ class LoginActivity: AppCompatActivity() {
                                     authDao.logout()
                                     binding.pbLogin.visibility = View.GONE
                                 }
-                            } else {                                // 이메일 인증이 안 된 경우
-                                val sendEmailResult = withContext(Dispatchers.IO) {
-                                    authDao.sendEmailForVerification()
-                                }
-
-                                if (sendEmailResult) {
-                                    binding.pbLogin.visibility = View.GONE
-                                    makeDialogforSendingVerificationEmail()
-                                    authDao.logout()
-                                } else {
-                                    Toast.makeText(this@LoginActivity, "An error has occurred. Please log in again.", Toast.LENGTH_SHORT).show()
-                                    authDao.logout()
-                                    binding.pbLogin.visibility = View.GONE
-                                }
-                            }
-                        } else {        // auth에 등록되지 않은 사용자인 경우
-                            if (checkIsItFirstPW(email, pw)) {
-                                val registerUserResult = withContext(Dispatchers.IO) {
-                                    authDao.registerUser(email, pw)
-                                }
-
-                                if (registerUserResult) {
-                                    val sendEmailResult = withContext(Dispatchers.IO) {
-                                        authDao.sendEmailForVerification()
-                                    }
-
-                                    if (sendEmailResult) {
-                                        binding.pbLogin.visibility = View.GONE
-                                        makeDialogforSendingVerificationEmail()
-                                    } else {
-                                        Toast.makeText(this@LoginActivity, "An error has occurred. Please log in again.", Toast.LENGTH_SHORT).show()
-                                        binding.pbLogin.visibility = View.GONE
-                                    }
-                                } else {
-                                    Toast.makeText(this@LoginActivity, "An error has occurred. Please log in again.", Toast.LENGTH_SHORT).show()
-                                    binding.pbLogin.visibility = View.GONE
-                                }
                             } else {
-                                Toast.makeText(this@LoginActivity, "Fail to login.", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(this@LoginActivity, "An error has occurred. Please log in again.", Toast.LENGTH_SHORT).show()
                                 binding.pbLogin.visibility = View.GONE
                             }
+                        } else {
+                            Toast.makeText(this@LoginActivity, "Fail to login.", Toast.LENGTH_SHORT).show()
+                            binding.pbLogin.visibility = View.GONE
                         }
                     } else if (temp.size == 0){
                         Toast.makeText(this@LoginActivity, "Fail to login.", Toast.LENGTH_SHORT).show()
@@ -253,20 +218,6 @@ class LoginActivity: AppCompatActivity() {
             setNegativeButton("NO") { dialog, which ->
                 dialog.dismiss()
                 changePage()
-            }
-        }
-
-        val alertDialog = alertDialogBuilder.create()
-        alertDialog.show()
-    }
-
-    fun makeDialogforSendingVerificationEmail() {
-        val alertDialogBuilder = AlertDialog.Builder(this)
-        alertDialogBuilder.apply {
-            setTitle("Verification Email")
-            setMessage(getString(R.string.verification_email_dialog_message))
-            setPositiveButton("OK") { dialog, which ->
-                null
             }
         }
 
